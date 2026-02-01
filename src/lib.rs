7f45d59e2f0a4bf92dad6c1ed6f1e38ae88fa19f
jianlong.chen <9114557@qq.com>
2026-02-01 23:11:19 +0800

    Initial commit: Solana project setup

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+solana_program::entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &solana_program::pubkey::Pubkey,
+    accounts: &[solana_program::account_info::AccountInfo],
+    data: &[u8],
+) -> solana_program::entrypoint::ProgramResult {
+    solana_program::msg!("Hello Solana!");
+    Ok(())
+}
